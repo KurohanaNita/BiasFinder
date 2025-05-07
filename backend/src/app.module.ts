fee1bfb947d34bce72ee1ee016a44e5f2b374f29
hliosone <sstelcher@protonmail.com>
2025-05-08 01:13:38 +0200

    Changed port to 4000 and implemented GoogleStrategy constructor + added env config in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: './.env',
+});
 
 @Module({
   imports: [UsersModule],
--- backend/src/auth/strategies/google.strategy.ts
+++ backend/src/auth/strategies/google.strategy.ts
@@ -3,4 +3,17 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth20';
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {}
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any) {
+    return await profile;
+  }
+}
